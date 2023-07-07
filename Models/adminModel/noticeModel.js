a8b963ef8bb9aa2d86285b5a6c985dbda6c54238
SuyogRedkar <76471004+SuyogRedkar@users.noreply.github.com>
2023-07-07 17:19:32 +0530

    Update noticeModel.js

--- Models/adminModel/noticeModel.js
+++ Models/adminModel/noticeModel.js
@@ -15,11 +15,11 @@ const noticeSchema = new mongoose.Schema({
     },
     filepath:{
         type:String,
-        required:true
+   //     required:true
     },
     upload_date: {
         type: Date,
-        required: true,
+       // required: true,
         default: Date.now
     }
 
